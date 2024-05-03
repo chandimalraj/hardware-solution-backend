13a740a31be351eed7255a91751e757a62a20df9
chandimalraj <chandimalrajapaksha97@gmail.com>
2024-05-03 16:09:16 +0530

    cors updated

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 8000
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
+// Enable CORS for all requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 const User = require('./models/user.model') 
 const Salesrep = require('./models/salesrep.model')
 const Item = require('./models/item.model') 
