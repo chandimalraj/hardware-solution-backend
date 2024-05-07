fbd774970e20876554c12f66e5ababfa5932d75f
chandimalraj <chandimalrajapaksha97@gmail.com>
2024-05-07 13:11:39 +0530

    changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Enable CORS for all requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
